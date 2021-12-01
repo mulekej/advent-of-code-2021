f96ecf5feca225dd9b5d7a4e40eded5613815a3e
Eric Mulek <eric.mulek@surescripts.com>
2021-12-01 13:03:05 -0600

    Initial commit

---
+++ src/main/groovy/com/ericmulek/adventofcode2021/AdventOfCode2021Application.groovy
@@ -0,0 +1,13 @@
+package com.ericmulek.adventofcode2021
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class AdventOfCode2021Application {
+
+    static void main(String[] args) {
+        SpringApplication.run(AdventOfCode2021Application, args)
+    }
+
+}
